d1805d914aceb32a4b9466a197a3456d47ac851c
Octavio <coronach95@gmail.com>
2017-09-20 14:01:25 -0500

    Add files via upload

---
+++ ordenamiento.f90
@@ -0,0 +1,39 @@
+program ordenamiento
+
+  implicit none
+  integer :: i,j,k
+  real(8) :: a
+  real :: M(10)
+
+  open(10,file="datos.dat")
+  do i=1,10
+    read(10,*)k,a
+    M(i)=a
+  enddo
+  close(10)
+
+  a=0
+
+  do i=1,10
+    do j=1,10
+      if (M(i) > M(j)) then
+        a = M(i)
+        M(i) = M(j)
+        M(j) = a
+      endif
+    enddo
+  enddo
+
+  do i=1,10
+    print*,M(i)
+  enddo
+
+  open(11,file="ordenados.dat")
+  do i=1,10
+    a = M(i)
+    write(11,*)i,a
+  enddo
+  close(11)
+
+
+end program ordenamiento
