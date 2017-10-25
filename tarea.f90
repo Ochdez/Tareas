5c6d53ae41815d9a0003641584b7f3d6ce5caf96
Octavio <coronach95@gmail.com>
2017-10-25 11:18:38 -0500

    Update tarea.f90

--- tarea.f90
+++ tarea.f90
@@ -1,11 +1,11 @@
 program main
 
-  !implicit none
+  implicit none
   real(8) :: pi, l, xmax, xmin
   real(8), allocatable, dimension(:) :: y,z,d,f
   integer :: m,n,p,r
 
-	pi = acos(-1.0)
+  pi = acos(-1.0)
   print*,"Ingrese un entero multiplo de PI para x minima"
   read(*,*)m
   print*,"Ingrese un entero multiplo de PI para x maxima"
@@ -13,8 +13,8 @@ program main
   print*,"Ingrese el numero de puntos"
   read(*,*)p
   allocate(y(0:p),z(0:p),d(0:p),f(0:p))
-	print*,"¿Cuantas veces deseas derivar la funcion? "
-	read(*,*)r
+  print*,"¿Cuantas veces deseas derivar la funcion? "
+  read(*,*)r
 
   xmin = m*pi
   xmax = n*pi
@@ -29,26 +29,13 @@ program main
   call fsub1(y,n,z)
   print*,"z=",z
 
-	!open(10,file="datos.dat") !!
-  !  do i=0,p
-  !    write(10,*)z(i)
-  !  enddo
-  !close(10)
-
-	do i=1, r
-		n=n-1
-  	call fsub2(z,d,n,l)
-  	print*,"derivada:",i
-		print*,d
-		z=d
-	enddo
-
-	!open(11,file="derivada.dat") !!
-  !  do i=0,p
-  !    write(11,*)d(i)
-  !  enddo
-  !close(11)
-
+  do i=1, r
+     n=n-1
+     call fsub2(z,d,n,l)
+     print*,"derivada:",i
+     print*,d
+     z=d
+  enddo
 
 end program main
 
@@ -67,7 +54,9 @@ end program main
     real(8), intent(in) :: l
     real(8), dimension(n), intent(in) :: x
     real(8), dimension(n), intent(out) :: d
+    
     do i=0,n
-			d(i) = (x(i+1)-x(i))/l
-		enddo
+       d(i) = (x(i+1)-x(i))/l
+    end do
+  
   end subroutine
