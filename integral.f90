0be6dfb131966ee7b2201fe165121369528fa687
Octavio <coronach95@gmail.com>
2018-02-01 16:20:33 -0600

    Update integral.f90

--- integral.f90
+++ integral.f90
@@ -3,8 +3,11 @@ program main
   !implicit none
   real(8) :: pi, dx, xmax, xmin, integral,a,b
   real(8), allocatable, dimension(:) :: y,z,f
-  integer :: p,h,n
+  integer :: p,h,n,r
   
+  pi = acos(-1.0)
+  print*,"Ingrese 1 si su función es trigonometria y otro entero si no lo es"
+  read(*,*)r
   print*,"Ingrese el punto a"
   read(*,*)a
   print*,"Ingrese el punto b"
@@ -14,7 +17,11 @@ program main
   p=h+1 
   allocate(y(0:p),z(0:p),f(0:p))
 
-  dx = (b-a)/p
+  if (r==1) then
+    dx = ((b*pi)-(a*pi))/p
+  else
+    dx = (b-a)/p
+  endif
 
   do i = 0, p
     y(i) = a + (i*dx) 
@@ -38,7 +45,7 @@ end program main
     real(8), dimension(0:n), intent(in) :: x
     real(8), dimension(0:n), intent(out) :: f
     do i=0,n
-	f(i)=x(i)*x(i)
+	f(i)=sin(x(i))
     enddo
 	
 
@@ -51,6 +58,10 @@ end program main
     real(8), dimension(0:n), intent(in) :: w 
     area=0
     do i=0,n
+      if (w(i)<0) then
+        area = area+(dx*w(i)*(-1.0))
+      else
         area = area+(dx*w(i))
+      endif
     enddo
   end subroutine  
