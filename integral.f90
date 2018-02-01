dc8b6ebdb126153043239c1e74fbe97854c83998
Octavio <coronach95@gmail.com>
2018-01-31 20:04:06 -0600

    Update integral.f90

--- integral.f90
+++ integral.f90
@@ -1,31 +1,31 @@
 program main
 
   !implicit none
-  real(8) :: pi, dx, xmax, xmin,area, integral
+  real(8) :: pi, dx, xmax, xmin, integral,a,b
   real(8), allocatable, dimension(:) :: y,z,f
-  integer :: p,h,a,b
-
+  integer :: p,h,n
+  
   print*,"Ingrese el punto a"
   read(*,*)a
   print*,"Ingrese el punto b"
   read(*,*)b
   print*,"Ingrese el numero de puntos"
   read(*,*)h
-  p=h+1
+  p=h+1 
   allocate(y(0:p),z(0:p),f(0:p))
 
   dx = (b-a)/p
 
   do i = 0, p
-    y(i) = a + (i*dx) !Puntos donde se evaluara la funcion
+    y(i) = a + (i*dx) 
   enddo
-
+ print*,y
   n = size(y)
 
   call fsub1(y,n,z)
   print*,"=",z
 
-  area = 0
+  
   call fsub2(z,n,dx,integral)
   print*,"integral=", integral
 
@@ -35,19 +35,22 @@ end program main
   subroutine fsub1(x,n,f)
 
     integer, intent(in) :: n
-    real(8), dimension(n), intent(in) :: x
-    real(8), dimension(n), intent(out) :: f
-    f = x*x
+    real(8), dimension(0:n), intent(in) :: x
+    real(8), dimension(0:n), intent(out) :: f
+    do i=0,n
+	f(i)=x(i)*x(i)
+    enddo
+	
 
   end subroutine
 
   subroutine fsub2(w,n,dx,area)
     integer, intent(in) :: n
     real(8), intent(in) :: dx
-    real(8), intent(out) :: area!!!!!!
-    real(8), dimension(n), intent(in) :: w 
-    
-    do i=0,(n-1)
+    real(8), intent(out) :: area
+    real(8), dimension(0:n), intent(in) :: w 
+    area=0
+    do i=0,n
         area = area+(dx*w(i))
     enddo
   end subroutine  
