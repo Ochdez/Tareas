2603b76bca526daf3e07487dac3bd3ebb59d40b0
Octavio <coronach95@gmail.com>
2018-02-09 17:03:36 -0600

    Update ceros.f90

--- ceros.f90
+++ ceros.f90
@@ -1,9 +1,13 @@
 program main
 
-   real(8) :: dx, xmax, xmin, integral,m,b,a,c
-   real(8), allocatable, dimension(:) :: x,z,f
    integer :: p,h,n,r,l
+   real(8) :: pi, dx,m,b,a,c,ds,t
+   real(8), allocatable, dimension(:) :: x,z,g
 
+   pi = acos(-1.0)
+   ds = 0.005
+   print*,"Su función es trigonometrica? si-1"
+   read(*,*)r
    print*,"Ingrese punto a: "
    read(*,*)a
    print*,"Ingrese punto b: "
@@ -11,33 +15,28 @@ program main
    print*,"Ingrese numero de puntos entre a y b: "
    read(*,*)h
    p=h+1
-   allocate(x(0:P),z(0:p))
+   allocate(x(0:P),z(0:p),g(0:50))
 
-   dx = (b-a)/p
- 
+   if (r==1)then
+   	dx = (b*pi-a*pi)/p
+   else
+   	dx = (b-a)/p
+   endif
+
    do i = 0, p
      x(i) = a + (i*dx)
-     print*,x(i)
    enddo
 
    print*," "
-   
+
    call fsub1(x,p,z)
-   do i=0,p
-       print*,z(i)
-   enddo
-   
+
    n=size(z)
-   print*,n
-   
+   print*,"n=",n
+
    print*," "
 
-   call fsub2(x,n,c,z)
-   print*,c
-   !do i=0,(p-1)
-	!print*,"Cero: ",cero(i)
-   !enddo
-   
+   call fsub2(x,n,c,z,ds,g,t)
 
 end program main
 
@@ -47,26 +46,41 @@ subroutine fsub1(y,p,f)
    real(8), dimension(0:p), intent(in) :: y
    real(8), dimension(0:p), intent(out) :: f
    do i=0,p
-	f(i)=((y(i)*y(i))-2)
+	    f(i)=(y(i)**2)-2
    enddo
 
 end subroutine
 
-subroutine fsub2(y,n,m,f)
+subroutine fsub2(x,p,m,f,ds,g,t)
 
-   integer, intent(in) :: n
+   integer, intent(in) :: p
    real(8), intent(out) :: m
-   real(8), dimension(0:n), intent(in) :: y
-   real(8), dimension(0:n), intent(in) :: f 
-   	
-   do i=0,n
+   real(8), intent(in) :: ds
+   real(8), intent(out) :: t
+   real(8), dimension(0:n), intent(in) :: x
+   real(8), dimension(0:n), intent(in) :: f
+   real(8), dimension(0:50), intent(out) :: g
+
+   do i=0,p
    	if ((f(i)*f(i+1))<0) then
-   		m=((y(i+1)-y(i))*(-f(i))/(f(i+1)-f(i)))+y(i)
+   		m=((x(i+1)-x(i))*(-f(i))/(f(i+1)-f(i)))+x(i)
    		print*,f(i)," ",f(i+1)
+   		print*,"Primera aproximacion de x =" ,m
+
+      do j=0,50
+        g(j)=(((m)+ds*j)**2)-2
+        if (g(j)>=0) then
+            print*,"Segunda aproximacion de x = ",t
+            exit
+        endif
+        t=((m)+ds*j)
+      enddo
+
+      exit
    	endif
    enddo
-   	
 
 end subroutine
+
 
 
